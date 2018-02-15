000a01c686ec2bff0254f211a8868b83bb4787d1
jsanchez819 <j0sanc02@gmail.com>
2018-02-15 10:39:04 -0500

    Fixed Email Issues

--- CallinAdjustments/CallinAdjustments/Module1.vb
+++ CallinAdjustments/CallinAdjustments/Module1.vb
@@ -30,12 +30,17 @@ Module Module1
 
             Using insertconnect As New SqlConnection(DBconnectionString)
                 Dim insertDBCommand As New SqlCommand
+                With insertDBCommand
+                    .Connection = insertconnect
+                    .Connection.Open()
 
-                For Each dr As DataRow In dt.Rows
+                    For Each dr As DataRow In dt.Rows
 
-                    With insertDBCommand
-                        .Connection = insertconnect
-                        .Connection.Open()
+                        Dim locationId As String = dr("LocationID")
+                        Dim itemid As Integer = CInt(dr("ItemID"))
+                        Dim quantity As Integer = CInt(dr("Quantity"))
+                        Dim userid As String = dr("UserID")
+
                         .CommandType = CommandType.StoredProcedure
                         .CommandText = "billing_InsertBillingAdjustment"
                         .Parameters.Clear()
@@ -50,33 +55,41 @@ Module Module1
                         End If
                         .Parameters.AddWithValue("@UserID", dr("UserID"))
                         .Parameters.AddWithValue("@Approved", 1)
+                        .ExecuteNonQuery()
 
-                    End With
-                Next
 
+                    Next
+                End With
             End Using
 
-            sendemail("Upload Complete!", "Call in orders have been copied to the Billing Adjustment Table")
-            logEvent("Success!", "Record upload to Billing Adjustment Table Completed!")
+            sendemail("LMS - Upload to Billing Adjustment Complete!", "Call in orders have been copied to the Billing Adjustment Table - " & Now)
+            logEvent("LMS - Success!", "Record upload to Billing Adjustment Table Completed! - " & Now)
 
         Catch ex As Exception
 
-            sendemail("Call in Order Error!", ex.ToString())
-            logEvent("Error", ex.ToString())
+            sendemail("LMS - Billing Adjustment Error!", Now & " - " & ex.ToString())
+            logEvent("Error", Now & " - " & ex.ToString())
 
         End Try
 
 
     End Sub
 
-    Private Sub sendemail(ByVal heading As String, ByVal result As String)
+    Private Sub sendemail(ByVal subject As String, ByVal mess As String)
         Dim SMTP As New SmtpClient(System.Configuration.ConfigurationManager.AppSettings("SMTPServer"))
         Dim message As New MailMessage(System.Configuration.ConfigurationManager.AppSettings("FromEmailAddress"), System.Configuration.ConfigurationManager.AppSettings("toEmailAddress"))
         Dim otherRecipients As String() = System.Configuration.ConfigurationManager.AppSettings("otherEmailAddresses").ToString().Split(",")
+        Dim username As String = System.Configuration.ConfigurationManager.AppSettings("username")
+        Dim password As String = System.Configuration.ConfigurationManager.AppSettings("password")
+        Dim port As Integer = CInt(System.Configuration.ConfigurationManager.AppSettings("port"))
 
+        SMTP.EnableSsl = True
 
-        message.Subject = heading
-        message.Body = result
+        SMTP.Credentials = New Net.NetworkCredential(username, password)
+        SMTP.Port = port
+
+        message.Subject = subject
+        message.Body = mess
 
         message.To.Clear()
         For Each Recipient As String In otherRecipients
@@ -85,21 +98,36 @@ Module Module1
 
         Try
             SMTP.Send(message)
-            logEvent("Success!", "Email Sent Complete!")
+            logEvent("Email Success", "Email Sent " & Now)
         Catch ex As Exception
-            logEvent("Error", ex.ToString())
+            logEvent("Email Error", Now & " - " & ex.ToString())
         End Try
 
     End Sub
 
     Private Sub logEvent(ByVal type As String, ByVal mess As String)
         If type = "Error" Then
+            System.IO.Directory.CreateDirectory(errorfilePath)
             Dim errorfilename As String = String.Format("BillingAdjustmentError{0:yyyyMMdd_HH-mm-ss}.txt", Date.Now)
             Dim errorfile = New StreamWriter(errorfilePath & errorfilename, True)
             errorfile.Write(mess)
             errorfile.Close()
 
+        ElseIf type = "Email Error" Then
+            System.IO.Directory.CreateDirectory(errorfilePath)
+            Dim errorfilename As String = String.Format("EmailError{0:yyyyMMdd_HH-mm-ss}.txt", Date.Now)
+            Dim errorfile = New StreamWriter(errorfilePath & errorfilename, True)
+            errorfile.Write(mess)
+            errorfile.Close()
+
+        ElseIf type = "Email Success" Then
+            System.IO.Directory.CreateDirectory(completefilePath)
+            Dim completefilename As String = String.Format("EmailSent{0:yyyyMMdd_HH-mm-ss}.txt", Date.Now)
+            Dim completefile = New StreamWriter(completefilePath & completefilename, True)
+            completefile.Write(mess)
+            completefile.Close()
         Else
+            System.IO.Directory.CreateDirectory(completefilePath)
             Dim completefilename As String = String.Format("BillingAdjustmentComplete{0:yyyyMMdd_HH-mm-ss}.txt", Date.Now)
             Dim completefile = New StreamWriter(completefilePath & completefilename, True)
             completefile.Write(mess)
